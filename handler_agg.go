994b0243fc0cfa203a0995479b89f81ca00e87f4
Kevin Hernot <kevin@labs.haystack.asia>
2025-12-21 21:30:54 +0700

    feat(agg): continuously fetch and aggregate RSS feeds

--- handler_agg.go
+++ handler_agg.go
@@ -3,16 +3,62 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"time"
 )
 
 func agg(s *state, c command) error {
-	url := "https://www.wagslane.dev/index.xml"
-	ctx := context.Background()
-	f, err := fetchFeed(ctx, url)
-	if err != nil {
-		return fmt.Errorf("couldn't fetch from %v: %w", url, err)
+	if len(c.args) != 1 {
+		return fmt.Errorf("usage: agg <timeBetweenReqs>")
 	}
-	fmt.Printf("feed: %v\n", f)
-	return nil
 
+	timeBetweenReqs := c.args[0]
+	timeInterval, err := time.ParseDuration(timeBetweenReqs)
+	if err != nil {
+		return fmt.Errorf("invalid duration %w", err)
+	}
+
+	fmt.Printf("Collecting feeds every %s\n", timeInterval)
+
+	ctx := context.Background()
+	ticker := time.NewTicker(timeInterval)
+	defer ticker.Stop()
+	for ; ; <-ticker.C {
+		if err := scrapeFeeds(s, ctx); err != nil {
+			log.Printf("scrape error: %v", err)
+		}
+	}
+}
+
+func scrapeFeeds(ctx context.Context, s *state) error {
+	nextFeed, err := s.db.GetNextFeedToFetch(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to fetch next feed: %w", err)
+	}
+
+	rssFeed, err := fetchFeed(ctx, nextFeed.Url)
+	if err != nil {
+		return fmt.Errorf("failed to fetch feed %s: %w", nextFeed.Name, err)
+	}
+
+	if err := s.db.MarkFeedFetched(ctx, nextFeed.ID); err != nil {
+		return fmt.Errorf("failed to mark feed as fetched: %w", err)
+	}
+
+	var feedTitle string
+	for i, item := range rssFeed.Channel.Item {
+		if item.Title == "" {
+			continue
+		}
+
+		if rssFeed.Channel.Title != feedTitle {
+			feedTitle = rssFeed.Channel.Title
+			fmt.Println()
+			fmt.Printf("Feed: %s\n", feedTitle)
+		}
+
+		fmt.Printf("> Blog %d: %s\n", i+1, item.Title)
+	}
+
+	return nil
 }
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -8,4 +8,14 @@ SELECT * FROM feeds;
 
 -- name: GetFeedByUrl :one
 SELECT * FROM feeds
-WHERE feeds.url = $1;
+WHERE url = $1;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET last_fetched_at=NOW(), updated_at=NOW()
+WHERE id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
