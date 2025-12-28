b342a35ee4c21b80f22eb4f8abea2e0276e8741e
Anoobis <heavykevin2000@hotmail.com>
2025-12-28 17:33:23 +0700

    refactor: return string for tui

--- internal/cli/commands.go
+++ internal/cli/commands.go
@@ -7,23 +7,23 @@ import (
 )
 
 type commands struct {
-	cmds map[string]func(*core.State, core.Command) error
+	cmds map[string]func(*core.State, core.Command) (string, error)
 }
 
 func NewCommand() *commands {
 	return &commands{
-		make(map[string]func(*core.State, core.Command) error),
+		make(map[string]func(*core.State, core.Command) (string, error)),
 	}
 }
 
-func (c commands) register(name string, f func(*core.State, core.Command) error) {
+func (c commands) register(name string, f func(*core.State, core.Command) (string, error)) {
 	c.cmds[name] = f
 }
 
-func (c commands) Run(s *core.State, cmd core.Command) error {
+func (c commands) Run(s *core.State, cmd core.Command) (string, error) {
 	handler, ok := c.cmds[cmd.Name]
 	if !ok {
-		return fmt.Errorf("unknown command: %s", cmd.Name)
+		return "", fmt.Errorf("unknown command: %s", cmd.Name)
 	}
 
 	return handler(s, cmd)
